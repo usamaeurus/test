a49a7554cf39a07ae147aaec8e8f66c6dd5c7ca4
Usama222000 <90505506+Usama222000@users.noreply.github.com>
2022-11-29 03:35:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Hello Usamas welcome to private repository aaaaaaaaaaa'));
+app.get('/', (req, res) => res.send('Hello Usamas welcome to private repository'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

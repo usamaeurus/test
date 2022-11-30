944f2360ff355fafe9e5095323555b169697c925
Usama222000 <90505506+Usama222000@users.noreply.github.com>
2022-12-01 00:43:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3100;
 
-app.get('/', (req, res) => res.send('Hello usama welcome to jenkins'));
+app.get('/', (req, res) => res.send('Hello usama welcome to jenkinsc,testing'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
